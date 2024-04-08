e7281ead6a93fdefb68b5a04eb9798853bf724b8
AbhilashEU <vemulaabhilash03@gmail.com>
2024-04-08 09:55:09 +0530

    Practiced and pushed the code

---
+++ src/test/java/org/CentricToAll1/Test/GeoFinder1.java
@@ -0,0 +1,42 @@
+package org.CentricToAll1.Test;
+
+import io.restassured.RestAssured;
+
+public class GeoFinder1
+{
+
+        public static void main(String args[]) {
+
+
+        //GET Request: https://api.zippopotam.us/IN/500030 and GET Method
+        //URL
+        //HEADER
+        //Base Url: https://api.zippopotam.us
+        //Base Path: /IN/500030
+        //Payload: Not required
+        //RESPONSE: Status code, Response body, Time, Headers and cookies
+
+
+        //Gerkin --> Given, When, Then and these are the BDD style
+        //BDD is a framework and it uses a syntax called GERKIN
+        //Non BDD framework and can use a Gerkin syntax.
+
+        //given(): Just like a pre-requisite before testing the API
+        //when():Test data required during execution
+        //then():Post testing, validation of response.
+
+
+        RestAssured
+                .given()
+                .baseUri("https://api.zippopotam.us").basePath("/IN/500030")
+  //Url is a combination of URI+Path
+
+                .when().log().all()
+                .get()
+
+                .then().log().all().statusCode(201);
+
+// log(). all() needs to be added post given() section. This will print the response body regardless if an error occurred. given().
+    }
+
+    }
